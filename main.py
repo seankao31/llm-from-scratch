3c80a84b6263e2b084eda57c37eb1490a03b8f9e
Sean Kao <seankao31@gmail.com>
2025-04-11 08:28:46 -0700

    test different model size
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- main.py
+++ main.py
@@ -22,7 +22,37 @@ def create_dataloader(txt, batch_size=4, max_length=256, stride=128, shuffle=Tru
     )
     return dataloader
 
+def test_model(cfg: GPTConfig):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    batch = []
+    txt1 = "Every effort moves you"
+    txt2 = "Every day holds a"
+
+    batch.append(torch.tensor(tokenizer.encode(txt1)))
+    batch.append(torch.tensor(tokenizer.encode(txt2)))
+    batch = torch.stack(batch, dim=0)
+    # print(batch)
+
+    torch.manual_seed(123)
+    model = GPTModel(cfg)
+    logits = model(batch)
+    print("Output shape:", logits.shape)
+    # print(logits)
+
+    att_params = sum(p.numel() for p in model.trf_blocks[0].att.parameters())
+    print(f"Number of parameters in one attention module: {att_params:,}")
+    ff_params = sum(p.numel() for p in model.trf_blocks[0].ff.parameters())
+    print(f"Number of parameters in one feed forward module: {ff_params:,}")
+
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Total number of parameters: {total_params:,}")
+    # Assume float32, 4 bytes per parameter
+    total_size_bytes = total_params * 4
+    total_size_mb = total_size_bytes / (1024 * 1024)
+    print(f"Total size of the model: {total_size_mb:.2f} MB")
+
 def main():
+    # GPT-2 small
     GPT_CONFIG_124M = GPTConfig(
         vocab_size=50257,
         context_length=1024,
@@ -32,22 +62,40 @@ def main():
         drop_rate=0.1,
         qkv_bias=False
     )
-    
-    tokenizer = tiktoken.get_encoding("gpt2")
-    batch = []
-    txt1 = "Every effort moves you"
-    txt2 = "Every day holds a"
 
-    batch.append(torch.tensor(tokenizer.encode(txt1)))
-    batch.append(torch.tensor(tokenizer.encode(txt2)))
-    batch = torch.stack(batch, dim=0)
-    print(batch)
+    GPT_CONFIG_MEDIUM = GPTConfig(
+        vocab_size=50257,
+        context_length=1024,
+        emb_dim=1024,
+        n_heads=16,
+        n_layers=24,
+        drop_rate=0.1,
+        qkv_bias=False
+    )
 
-    torch.manual_seed(123)
-    model = GPTModel(GPT_CONFIG_124M)
-    logits = model(batch)
-    print("Output shape:", logits.shape)
-    print(logits)
+    GPT_CONFIG_LARGE = GPTConfig(
+        vocab_size=50257,
+        context_length=1024,
+        emb_dim=1280,
+        n_heads=20,
+        n_layers=36,
+        drop_rate=0.1,
+        qkv_bias=False
+    )
+
+    GPT_CONFIG_XL = GPTConfig(
+        vocab_size=50257,
+        context_length=1024,
+        emb_dim=1600,
+        n_heads=25,
+        n_layers=48,
+        drop_rate=0.1,
+        qkv_bias=False
+    )
+
+    configs = [GPT_CONFIG_124M, GPT_CONFIG_MEDIUM, GPT_CONFIG_LARGE, GPT_CONFIG_XL]
+    for cfg in configs:
+        test_model(cfg)
 
 if __name__ == "__main__":
     main()
