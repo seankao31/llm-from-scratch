58ade931df9d68f2e58ba98b9f5af68770abdf07
Sean Kao <seankao31@gmail.com>
2025-04-13 15:24:32 -0700

    plot losses
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tiktoken
 import torch
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from torch.utils.data import DataLoader
 from gpt_dataset import GPTDataset
 from gpt_config import GPTConfig
@@ -194,6 +196,22 @@ def train_model(
         generate_and_print_sample(model, tokenizer, device, start_context)
     return train_losses, val_losses, track_tokens_seen
 
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    ax2 = ax1.twiny()
+    # Invisble plot for aligning ticks
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    plt.show()
+
 def main():
     file_path = "the-verdict.txt"
     text_data = load_file(file_path)
@@ -241,6 +259,8 @@ def main():
             start_context="Every effort moves you"
         )
         timer.mark("Train completed")
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
 
 if __name__ == "__main__":
     main()
