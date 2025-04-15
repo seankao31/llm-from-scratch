6d9ae4e549858550918941ee42e2ee8c999ad694
Sean Kao <seankao31@gmail.com>
2025-04-15 09:27:29 -0700

    load pretrained weights from OpenAI
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import tiktoken
 import torch
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+import numpy as np
 from torch.utils.data import DataLoader
 from gpt_dataset import GPTDataset
+from gpt_download import download_and_load_gpt2
 from gpt_config import GPTConfig
 from gpt_model import GPTModel
 from segmented_timer import SegmentedTimer
@@ -15,7 +17,7 @@ if torch.cuda.is_available():
 else:
     DEVICE = torch.device("cpu")
 
-# Smaller model for training on single laptop
+# Smaller context length for training on single laptop
 GPT_CONFIG_124M_MINI = GPTConfig(
     vocab_size=50257,
     context_length=256,
@@ -28,54 +30,56 @@ GPT_CONFIG_124M_MINI = GPTConfig(
     qkv_bias=False
 )
 
-# GPT-2 small
-GPT_CONFIG_124M = GPTConfig(
-    vocab_size=50257,
-    context_length=1024,
-    emb_dim=768,
-    n_heads=12,
-    n_layers=12,
-    drop_rate_attn=0.1,
-    drop_rate_shortcut=0.1,
-    drop_rate_emb=0.1,
-    qkv_bias=False
-)
-
-GPT_CONFIG_MEDIUM = GPTConfig(
-    vocab_size=50257,
-    context_length=1024,
-    emb_dim=1024,
-    n_heads=16,
-    n_layers=24,
-    drop_rate_attn=0.1,
-    drop_rate_shortcut=0.1,
-    drop_rate_emb=0.1,
-    qkv_bias=False
-)
-
-GPT_CONFIG_LARGE = GPTConfig(
-    vocab_size=50257,
-    context_length=1024,
-    emb_dim=1280,
-    n_heads=20,
-    n_layers=36,
-    drop_rate_attn=0.1,
-    drop_rate_shortcut=0.1,
-    drop_rate_emb=0.1,
-    qkv_bias=False
-)
-
-GPT_CONFIG_XL = GPTConfig(
-    vocab_size=50257,
-    context_length=1024,
-    emb_dim=1600,
-    n_heads=25,
-    n_layers=48,
-    drop_rate_attn=0.1,
-    drop_rate_shortcut=0.1,
-    drop_rate_emb=0.1,
-    qkv_bias=False
-)
+GPT_CONFIGS = {
+    # Small
+    "124M": GPTConfig(
+        vocab_size=50257,
+        context_length=1024,
+        emb_dim=768,
+        n_heads=12,
+        n_layers=12,
+        drop_rate_attn=0.1,
+        drop_rate_shortcut=0.1,
+        drop_rate_emb=0.1,
+        qkv_bias=False
+    ),
+    # Medium
+    "355M": GPTConfig(
+        vocab_size=50257,
+        context_length=1024,
+        emb_dim=1024,
+        n_heads=16,
+        n_layers=24,
+        drop_rate_attn=0.1,
+        drop_rate_shortcut=0.1,
+        drop_rate_emb=0.1,
+        qkv_bias=False
+    ),
+    # Large
+    "774M": GPTConfig(
+        vocab_size=50257,
+        context_length=1024,
+        emb_dim=1280,
+        n_heads=20,
+        n_layers=36,
+        drop_rate_attn=0.1,
+        drop_rate_shortcut=0.1,
+        drop_rate_emb=0.1,
+        qkv_bias=False
+    ),
+    # XL
+    "1558M": GPTConfig(
+        vocab_size=50257,
+        context_length=1024,
+        emb_dim=1600,
+        n_heads=25,
+        n_layers=48,
+        drop_rate_attn=0.1,
+        drop_rate_shortcut=0.1,
+        drop_rate_emb=0.1,
+        qkv_bias=False
+    )
+}
 
 TOKEN_END_OF_TEXT = '<|endoftext|>'
 
@@ -235,7 +239,7 @@ def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
     fig.tight_layout()
     plt.show()
 
-def main():
+def test_train():
     file_path = "the-verdict.txt"
     text_data = load_file(file_path)
 
@@ -284,6 +288,94 @@ def main():
         timer.mark("Train completed")
     epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
     plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
+    return torch.nn.Parameter(torch.tensor(right))
+
+def load_weights_into_gpt(gpt: GPTModel, params: dict[str, list[dict]]):
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
+
+    for b in range(len(params["blocks"])):
+        q_w, k_w, v_w = np.split((params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.weight = assign(gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+        gpt.trf_blocks[b].att.W_key.weight = assign(gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+        gpt.trf_blocks[b].att.W_value.weight = assign(gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+
+        q_b, k_b, v_b = np.split((params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.bias = assign(gpt.trf_blocks[b].att.W_query.bias, q_b)
+        gpt.trf_blocks[b].att.W_key.bias = assign(gpt.trf_blocks[b].att.W_key.bias, k_b)
+        gpt.trf_blocks[b].att.W_value.bias = assign(gpt.trf_blocks[b].att.W_value.bias, v_b)
+
+        gpt.trf_blocks[b].att.out_proj.weight = assign(
+            gpt.trf_blocks[b].att.out_proj.weight,
+            params["blocks"][b]["attn"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].att.out_proj.bias = assign(
+            gpt.trf_blocks[b].att.out_proj.bias,
+            params["blocks"][b]["attn"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+            gpt.trf_blocks[b].ff.layers[0].weight,
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            gpt.trf_blocks[b].ff.layers[0].bias,
+            params["blocks"][b]["mlp"]["c_fc"]["b"])
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(
+            gpt.trf_blocks[b].ff.layers[2].weight,
+            params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(
+            gpt.trf_blocks[b].ff.layers[2].bias,
+            params["blocks"][b]["mlp"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].norm1.scale = assign(
+            gpt.trf_blocks[b].norm1.scale,
+            params["blocks"][b]["ln_1"]["g"])
+        gpt.trf_blocks[b].norm1.shift = assign(
+            gpt.trf_blocks[b].norm1.shift,
+            params["blocks"][b]["ln_1"]["b"])
+        gpt.trf_blocks[b].norm2.scale = assign(
+            gpt.trf_blocks[b].norm2.scale,
+            params["blocks"][b]["ln_2"]["g"])
+        gpt.trf_blocks[b].norm2.shift = assign(
+            gpt.trf_blocks[b].norm2.shift,
+            params["blocks"][b]["ln_2"]["b"])
+
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    # Weight tying. Reuse token embedding weights in the output layer
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+
+def main():
+    model_size = "124M"
+    assert model_size in GPT_CONFIGS.keys()
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    device = DEVICE
+
+    settings, params = download_and_load_gpt2(model_size=model_size, models_dir="gpt2")
+    cfg = GPT_CONFIGS[model_size]
+    # Bias vectors are not commonly used in LLMs anymore.
+    # However, it was used for the pretrained GPT2 weights.
+    cfg.qkv_bias = True
+
+    gpt = GPTModel(cfg)
+    gpt.eval()
+    load_weights_into_gpt(gpt, params)
+    gpt.to(device)
+
+    torch.manual_seed(123)
+    token_ids = generate_text(
+        model=gpt,
+        idx=text_to_token_ids("Every effort moves you", tokenizer).to(device),
+        max_new_tokens=25,
+        context_size=cfg.context_length,
+        top_k=50,
+        temperature=1.5
+    )
+    print("Output text:")
+    print(token_ids_to_text(token_ids, tokenizer))
 
 if __name__ == "__main__":
     main()
